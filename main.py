c679800d563e3869705a2ff0e3fc4adc5d0753f1
studhubeal7667 <alexander.huber_4@stud.th-rosenheim.de>
2024-11-06 11:07:40 +0100

    added multichat

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ import socket
 import sys
 import threading
 import socketserver
+import os
 
 UDP_IP = "141.60.1.1"
 CHATTERS = []
 UDP_PORT = 1141
 USERNAME = "Alex"
+SERVER_IP = "0"
 
 USERNAME = input("Enter your Username: ")
 
+if(sys.argv[1] != ""):
+    CHATTERS.append(sys.argv[1])
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto(bytes(USERNAME + " joined the Chat.", "utf-8"), (UDP_IP, UDP_PORT))
-
-print(sock.getsockname()[0])
 
 MESSAGE = "(" + USERNAME + "): "
 for i in range(1, len(sys.argv)):
@@ -25,12 +28,28 @@ class MyUDPHandler(socketserver.DatagramRequestHandler):
         msgRecvd = self.rfile.readline().strip()
         if(not self.client_address[0] in CHATTERS):
             CHATTERS.append(self.client_address[0])
+            send_message("/" + self.client_address[0])
         print(self.client_address[0] + msgRecvd.decode("utf-8"))
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 def send_message(message):
     if(message[0] == "/"):
         if(message == "/exit"):
             exit()
+        else:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+            sock.sendto(bytes(message, "utf-8"), (i, UDP_PORT))
     else:
         for i in CHATTERS:
             message = "(" + USERNAME + "): " + message
@@ -44,6 +63,10 @@ def background():
 
     serverUDP = socketserver.UDPServer(listen_addr, MyUDPHandler)
     serverUDP.serve_forever()
+
+SERVER_IP = get_ip()
+print("Host IP Address: " + SERVER_IP)
+CHATTERS.append(SERVER_IP)
 
 b = threading.Thread(name='background', target=background)
 
